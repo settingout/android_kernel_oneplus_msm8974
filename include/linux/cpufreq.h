e0c0828592ca2d78c975bbb882ec01ee678addc1
jgcaap <jgcaap@gmail.com>
2015-03-28 19:44:32 +0000

    added missing part for franco interactive gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -385,6 +385,10 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
 #endif
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE_FRANCO)
+extern struct cpufreq_governor cpufreq_gov_interactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive_franco)
+#endif
 
 
 /*********************************************************************
